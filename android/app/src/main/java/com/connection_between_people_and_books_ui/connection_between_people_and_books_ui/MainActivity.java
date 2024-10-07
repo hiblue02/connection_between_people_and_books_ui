a8674325bd8c339ba48a3072114fbfe065a737c6
hiblue02 <hiblue02@gmail.com>
2024-10-07 15:38:42 +0900

    Initial commit

---
+++ android/app/src/main/java/com/connection_between_people_and_books_ui/connection_between_people_and_books_ui/MainActivity.java
@@ -0,0 +1,6 @@
+package com.connection_between_people_and_books_ui.connection_between_people_and_books_ui;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
